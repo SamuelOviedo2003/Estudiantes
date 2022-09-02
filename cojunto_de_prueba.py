d5097592bbbf7c059cf3655f1834e1c462e662ae
IsisAmaya <100328242+IsisAmaya@users.noreply.github.com>
2022-09-02 10:44:49 -0500

    Add files via upload

---
+++ cojunto_de_prueba.py
@@ -0,0 +1,27 @@
+from pickletools import int4
+import random
+
+
+def AllRandom(cant: int):
+    lista2 = []
+    for i in range(cant):
+        A = [
+        (random.randint(0,1)),
+        (random.randint(1,32)),	
+        (random.randint(1,11))	
+        ]
+        lista2.append(A)
+    #print (lista2)
+    return lista2
+
+
+#Valor normalizado = (Valor - Característica mínima)/(Característica máxima - Característica mínima)
+
+def DatosPruebaRed(lista2):
+    decF = []
+    for i in range(len(lista2)):
+        dec = [((lista2[i][2]+lista2[i][1]+lista2[i][0])*2.27)/100]
+        decF.append(dec)
+        
+    #print (decF)
+    return decF
