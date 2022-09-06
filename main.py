004a1ce75329372106c334fb894adfd4d2d961f2
gotaluism <76192117+gotaluism@users.noreply.github.com>
2022-09-05 21:06:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 
 
+
 from dato import *
 from datos import *
 from generar import *
 from redesNeuronalesActual import *
+from graficos import *
 
 
 def prueba(cantidad):
@@ -12,17 +14,29 @@ def prueba(cantidad):
     for item in muestra:
         print(f'[{i}] : {item}')
         i += 1
-    ordenado=ordenandoDatosNecesarios(muestra)
-    ordenadoCompleto=ordenado[0]
+    
+    ordenado=ordenandoDatosNecesarios(muestra)      #Esto ordena el [0,25,2] etc.. (para poder sacar resultado en la red neuronal)
+    ordenadoCompleto=ordenado[0]             
     ordenadoDesertor=ordenado[1]
+    
+    ordenadoDict=ordenandoDatosNecesariosDict(muestra)  #Esto clasifica por desertores y no desertores pero en el arreglo mismo
+    ordenadoDesertor00=ordenadoDict[0]
+    ordenadoDesertorNo00=ordenadoDict[1]
+    
+    
     
     
     #Aqui calcula el porcentaje de cada estudiante que se graduen de la universidad, (solo están los estudiantes que se graduaron)
-    entrenandoMaquinaCompleto(ordenadoCompleto)
+    desertor0Red=entrenandoMaquinaCompleto(ordenadoCompleto)
     
     #Aqui calcula el porcentaje de cada estudiante que vuelva al colegio, (solo están los estudiantes que desertaron)
-    entrenandoMaquinaDesertores(ordenadoDesertor)
+    desertorNo0Red=entrenandoMaquinaDesertores(ordenadoDesertor)
+    #invocacion de los metodos anteriores
+    arreglo_definitivoDesertores = busqueda(inginieria_inversa(ordenadoDesertor00,desertor0Red)) 
+    grafica_definitiva(arreglo_definitivoDesertores)
 
+    arreglo_definitivoNoDesertores = busqueda(inginieria_inversa(ordenadoDesertorNo00,desertorNo0Red)) 
+    grafica_definitiva(arreglo_definitivoDesertores)
 
 
 if __name__ == "__main__":
@@ -30,6 +44,6 @@ if __name__ == "__main__":
     print("Generar Conjunto de datos. ")
     prueba(int(input("Cantidad de datos a generar? ")))
     print("-"*50)
-
+    
 
 
