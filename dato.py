35e3a48b57fcb0f75f39a6996d63bb5fc6068f4f
gotaluism <76192117+gotaluism@users.noreply.github.com>
2022-09-05 21:08:21 -0500

    Update dato.py

--- dato.py
+++ dato.py
@@ -1,44 +1,42 @@
 from generar import *
 
-
 class Dato:
-    def __init__(self, muestra):
-        dato = muestra
+    def __init__(self,muestra):
+        dato = generarDato()
         self.nombre = dato['nombre']
         self.sexo = dato['sexo']
         self.edad = dato['edad']
-        self.departamento = dato['departamento']
+        self.departamento= dato['departamento']
         self.municipio = dato['municipio']
         self.grados_Academico = dato['grados_Academico']
-        self.anos_Actual = dato['anos_Actual']
-        self.anos_Estudio = dato['anos_Estudio']
-        self.ano_Reprobados = dato['anos_Reprobados']
         self.desercion = dato['desercion']
-        self.porcentaje = dato['porcentaje']
+        self.anos_Estudio = dato['anos_Estudio']
+        #self.anos_Actual = dato['anos_Actual']            #isis
+        self.ano_Reprobados = dato['anos_Reprobados']
+        self.porcentaje= dato['porcentaje']                 #isis
+#==
+    def __eq__ (self, __o: object) -> bool:
+        return (self.edad == __o.edad) #and (self.sexo == __o.sexo)
 
-    # ==
-    def __eq__(self, __o: object) -> bool:
-        return (self.edad == __o.edad)  # and (self.sexo == __o.sexo)
+#<
+    def __lt__ (self, __o: object) -> bool:
+        return (self.edad< __o.edad) #and (self.sexo == __o.sexo)
 
-    # <
-    def __lt__(self, __o: object) -> bool:
-        return (self.edad < __o.edad)  # and (self.sexo == __o.sexo)
+#<=
+    def __le__ (self, __o: object) -> bool:
+        return (self.edad <= __o.edad) #and (self.sexo == __o.sexo)
 
-    # <=
-    def __le__(self, __o: object) -> bool:
-        return (self.edad <= __o.edad)  # and (self.sexo == __o.sexo)
+#>
+    def __gt__ (self, __o: object) -> bool:
+        return (self.edad > __o.edad) #and (self.sexo == __o.sexo)
 
-    # >
-    def __gt__(self, __o: object) -> bool:
-        return (self.edad > __o.edad)  # and (self.sexo == __o.sexo)
+#>=
+    def __ge__ (self, __o: object) -> bool:
+        return (self.edad >= __o.edad) #and (self.sexo == __o.sexo)
 
-    # >=
-    def __ge__(self, __o: object) -> bool:
-        return (self.edad >= __o.edad)  # and (self.sexo == __o.sexo)
-
-    #!=
-    def __ne__(self, __o: object) -> bool:
-        return (self.edad != __o.edad)  # or (self.sexo != __o.sexo)
+#!=
+    def __ne__ (self, __o: object) -> bool:
+        return (self.edad != __o.edad) #or (self.sexo != __o.sexo)
 
     def __str__(self):
-        return f'Objeto Dato=[nombre: {self.nombre}, edad: {self.edad}, sexo:{self.sexo},departamento:{self.departamento},municipio:{self.municipio},anos_Actual:{self.anos_Actual}, desercion:{self.desercion}, porcentaje:{self.porcentaje}]'
+        return f'Objeto Dato=[nombre: {self.nombre}, edad: {self.edad}, sexo:{self.sexo},departamento:{self.departamento},municipio:{self.municipio},desercion:{self.desercion},anos_reprobados:{self.ano_Reprobados},porcentaje:{self.porcentaje}]'
